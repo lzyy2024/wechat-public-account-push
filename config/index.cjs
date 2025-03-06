d2b24062824f0b040571907127ef39d7c2cf6553
lzy <2972013149@qq.com>
2025-03-06 15:59:02 +0800

    Update index.cjs

--- config/index.cjs
+++ config/index.cjs
@@ -81,7 +81,7 @@ const USER_CONFIG = {
       // 想要发送的人的名字
       name: '丁轶轩',
       // 使用微信测试号：扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
-      id: 'oOPf17LpB5hIK5G0rSmqPwpqdkuk',
+      id: 'oOPf17CXs_xS4OOSTHWSlyC3n0VA',
       // 使用微信测试号：你想对他发送的模板消息的模板ID
       useTemplateId: 'gfoWLrdzTDqHhsffP4v4G126n4L1MCBhaS8Biy0a_cA',
       // 新历生日, 仅用作获取星座运势, 格式必须为MM-DD
